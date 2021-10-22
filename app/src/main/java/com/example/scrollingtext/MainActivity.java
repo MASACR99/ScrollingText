aa86f696bfedac536c1457329583128a3b80736d
MASACR99 <joangil99@gmail.com>
2021-10-22 18:20:15 +0200

    Coding challenge

--- app/src/main/java/com/example/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.scrollingtext;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button edit = findViewById(R.id.editTextButton);
+        EditText text = findViewById(R.id.article);
+        text.setFocusable(false);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                EditText text = findViewById(R.id.article);
+                if(text.getFocusable() == View.NOT_FOCUSABLE){
+                    edit.setText("Save");
+                    text.setFocusableInTouchMode(true);
+                }else {
+                    edit.setText("Edit");
+                    text.setFocusable(false);
+                }
+            }
+        });
     }
 }
